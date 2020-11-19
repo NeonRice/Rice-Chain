5d453a5a19be87ac7da57fd0666aeea0fc9a14b1
NeonRice <zanas.kovaliovas@inbox.lt>
2020-11-19 10:09:23 +0200

    Commiting this so I can continue at work

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <random>
 #include <iostream>
 
 #include "../external/hash.h"
@@ -5,19 +6,31 @@
 #include "../headers/Transaction.h"
 #include "../headers/User.h"
 
+std::vector<Transaction*> pickRandTransactions(const unsigned &amount, std::vector<Transaction> &transactions)
+{
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_int_distribution<int> distr(0, transactions.size());
+
+    std::vector<Transaction*> pickedTransactions;
+
+    for (size_t i = 0; i < amount; i++)
+    {
+        int transactionNr = distr(generator);
+        pickedTransactions.push_back(&transactions[transactionNr]);
+    }
+
+    return pickedTransactions;
+} 
+
 int main()
 {
     std::cout << "Generating users..." << std::endl;
     std::vector<User> userPool = generateUsers(1000);
     std::cout << "Generating transactions..." << std::endl;
     std::vector<Transaction> transactionPool = generateTransactions(10000, userPool);
-    
-    for (auto &&transaction : transactionPool)
-    {
-        std::cout << transaction.getSender() << " " << transaction.getReceiver() << std::endl;
-        std::cout << transaction.transaction_id << std::endl;
-    }
-    
+    std::vector<Transaction*> pickedTransactions = pickRandTransactions(100, transactionPool);
+    std::cout << pickedTransactions.size() << std::endl;    
 
     return 0;
 }
